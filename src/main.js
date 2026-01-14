56a8ebbaaefe58f2735d046ad83a6a74ede5486a
Petrik Diák <petrik@petrik.hu>
2026-01-14 11:48:29 +0100

    Javascript elkészítve, kis változattások a html-ben.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-
+const form = document.getElementById('registerForm');
+      const sikeres = document.getElementById('app');
+      form.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        form.reset();
+        sikeres.innerHTML = '<div class="alert alert-success">Sikeres regisztráció</div>';
+      });
